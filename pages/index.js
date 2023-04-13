fdd79672a41c60d58336db9d465bbbaba950c067
sumitchowdhary <sumitchowdhary@matrixnmedia.com>
2023-04-13 14:51:35 +0530

    message

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const index = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Home Page</h1>
+      <h1 className="text-3xl font-bold">Home Page hai</h1>
       <Link href="/Product">PROFILE....</Link>
     </div>
   );
